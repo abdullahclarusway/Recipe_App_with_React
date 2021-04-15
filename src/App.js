ee43b3ee05282cb866e614ec58630842903eb936
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-04-15 23:43:03 +0200

    Get data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const APP_ID = "e986084e";
+  const APP_KEY = "d9c1485cbfc9eb0fae2acd8c251dea51";
+  // const [data, setData] = useState();
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+  };
   return (
     <div>
-      <h1>Let's build a recipe app</h1>
+      <form className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
